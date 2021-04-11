2b00b99b8a10f95bd767558e4d45a542d73198bd
carina1754 <bulebed1754@gmail.com>
2021-04-11 22:55:15 +0900

    1

---
+++ cnn-lstm.py
@@ -0,0 +1,58 @@
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D
+from keras.datasets import imdb
+import pandas as pd
+import numpy
+import tensorflow as tf
+import matplotlib.pyplot as plt
+  
+train = pd.read_csv('1.csv')
+test = pd.read_csv('2.csv')
+
+print(train.shape)
+print(test.shape)
+
+# from sklearn.model_selection import train_test_split
+# x_train,x_test = train_test_split(train, test_size=0.3, random_state=0)
+
+# # 모델의 설정
+# model = Sequential()
+# model.add(Embedding(5000, 100))
+# model.add(Dropout(0.5))
+# model.add(Conv1D(64, 5, activation='relu'))
+# model.add(MaxPooling1D(pool_size=4))
+# model.add(LSTM(55))
+# model.add(Dense(1))
+# model.add(Activation('sigmoid'))
+# model.summary()
+  
+# # 모델의 컴파일
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  
+# # 모델의 실행
+# history = model.fit(x_train, batch_size=100, epochs=5)
+  
+# # 테스트 정확도 출력
+# print("\n Test Accuracy: %.4f" % (model.evaluate(x_test)[1]))
+  
+# # 테스트셋의 오차
+# y_vloss = history.history['val_loss']
+  
+# # 학습셋의 오차
+# y_loss = history.history['loss']
+  
+# # 그래프로 표현
+# x_len = numpy.arange(len(y_loss))
+# plt.plot(x_len, y_vloss, marker='.', c="red", label='Testset_loss')
+# plt.plot(x_len, y_loss, marker='.', c="blue", label='Trainset_loss')
+  
+# # 그래프에 그리드를 주고 레이블을 표시
+# plt.legend(loc='upper right')
+# plt.grid()
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.show()
--- dataframe.py
+++ dataframe.py
@@ -2,37 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
-
-url = "https://news.naver.com/main/factcheck/main.nhn?section=%C4%DA%B7%CE%B3%AA+%B9%D9%C0%CC%B7%AF%BD%BA"
-headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
-html = requests.get(url, headers = headers)
-soup = BeautifulSoup(html.text, 'html.parser')
-titles_html = soup.select('.section > .talk_area >.info_area > .txt > a')
-titles_level = soup.select('.bx .blind')
-data = []
-for i in range(len(titles_level)):
-    titles_level[i] = titles_level[i].text
-    titles_html[i] = titles_html[i].text
-    data += [[titles_html[i],titles_level[i]]]
-
-dataframe = pd.DataFrame(data,columns=['title', 'level'])
-dataframe.to_csv("1.csv",header=False,index=False,encoding='cp949')
-
-print(data)
-
-for i in range(2,32):
-    url = "https://news.naver.com/main/factcheck/main.nhn?section=%C4%DA%B7%CE%B3%AA%B9%E9%BD%C5".format(i)
+data=[]
+for i in range(0,50):
+    url = "https://search.naver.com/search.naver?&where=news&query=%EC%BD%94%EB%A1%9C%EB%82%98%20%EB%89%B4%EC%8A%A4&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:dd,p:all,a:all&mynews=0&start={}&refresh_start=0".format(i*10+1)
     headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
     html = requests.get(url, headers = headers)
     soup = BeautifulSoup(html.text, 'html.parser')
-    titles_html = soup.select('.section > .talk_area >.info_area > .txt > a')    
-    titles_level = soup.select('.bx .blind')
-    for i in range(len(titles_level)):
-        titles_level[i] = titles_level[i].text
+    titles_html = soup.select('.news_area > a')
     for i in range(len(titles_html)):
-        titles_html[i] = titles_html[i].text
+        titles_html[i] = titles_html[i]
+        print(titles_html[i]['title'])
     for i in range(len(titles_html)):
-        data += [[titles_html[i],titles_level[i]]]
+        data += [[titles_html[i]['title'],0]]
 
     dataframe = pd.DataFrame(data,columns=['title', 'level'])
-    dataframe.to_csv("1.csv",header=False,index=False,encoding='cp949')
+    dataframe.to_csv("2.csv",header=False,index=False,encoding='utf-8')
