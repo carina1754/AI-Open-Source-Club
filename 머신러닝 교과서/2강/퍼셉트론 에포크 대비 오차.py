127b1ea8492af3a6ee0a35605f4a5b9915bf6502
huiseung5571 <huiseung5571@naver.com>
2021-05-07 18:56:45 +0900

    fourth

---
+++ 머신러닝 교과서/2강/퍼셉트론 에포크 대비 오차.py
@@ -0,0 +1,6 @@
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
+plt.show()
